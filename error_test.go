67d90282ab19041f103bea65bf6f0aaca2d3e00d
Michael Kenney <mkenney@webbedlam.com>
2019-07-17 23:21:59 -0600

    update tests

--- error_test.go
+++ error_test.go
@@ -1,6 +1,7 @@
 package errors_test
 
 import (
+	"encoding/json"
 	go_errors "errors"
 	"testing"
 
@@ -9,8 +10,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
-var testError = errors.Errorf("Test error")
 
 func init() {
 	log.SetLevel(log.DebugLevel)
@@ -24,7 +23,7 @@ func init() {
 	})
 }
 
-func TestEHas(t *testing.T) {
+func TestHas(t *testing.T) {
 	var err error
 	var testErr error
 	assert := assert.New(t)
@@ -50,7 +49,7 @@ func TestEHas(t *testing.T) {
 	assert.False(errors.Has(nil, testErr), "nil did not evaluate to false")
 }
 
-func TestEIs(t *testing.T) {
+func TestIs(t *testing.T) {
 	var err error
 	var testErr error
 	assert := assert.New(t)
@@ -75,31 +74,65 @@ func TestEIs(t *testing.T) {
 	assert.False(errors.Is(nil, testErr), "nil did not evaluate to false")
 }
 
-func TestETrace(t *testing.T) {
+func TestTrace(t *testing.T) {
 	assert := assert.New(t)
 
 	err := errors.New("test 1")
 	err = errors.Trace(err)
 
-	assert.Equal(82, err.Caller().Line(), "caller did not reflect the correct line number")
-	assert.Equal("github.com/bdlm/errors_test.TestETrace", err.Caller().Func(), "caller did not reflect the correct function name")
+	assert.Equal(81, err.Caller().Line(), "caller did not reflect the correct line number")
+	assert.Equal("github.com/bdlm/errors_test.TestTrace", err.Caller().Func(), "caller did not reflect the correct function name")
 
 	err = errors.Trace(nil)
 	assert.Equal(nil, err, "err is not nil")
 }
 
-func TestETrack(t *testing.T) {
+func TestTrack(t *testing.T) {
 	assert := assert.New(t)
 
 	err := errors.New("test 1")
 	err = errors.Track(err)
 
-	assert.Equal(94, err.Caller().Line(), "caller did not reflect the correct line number")
-	assert.Equal("github.com/bdlm/errors_test.TestETrack", err.Caller().Func(), "caller did not reflect the correct function name")
+	assert.Equal(93, err.Caller().Line(), "caller did not reflect the correct line number")
+	assert.Equal("github.com/bdlm/errors_test.TestTrack", err.Caller().Func(), "caller did not reflect the correct function name")
 
-	assert.Equal(95, errors.Unwrap(err).Caller().Line(), "caller did not reflect the correct line number")
-	assert.Equal("github.com/bdlm/errors_test.TestETrack", errors.Unwrap(err).Caller().Func(), "caller did not reflect the correct function name")
+	assert.Equal(94, errors.Unwrap(err).Caller().Line(), "caller did not reflect the correct line number")
+	assert.Equal("github.com/bdlm/errors_test.TestTrack", errors.Unwrap(err).Caller().Func(), "caller did not reflect the correct function name")
 
 	err = errors.Track(nil)
 	assert.Equal(nil, err, "err is not nil")
+}
+
+func TestErrorf(t *testing.T) {
+	assert := assert.New(t)
+
+	err := errors.Errorf("test %d", 1)
+	assert.Equal("test 1", err.Error(), "err is not 'test 1'")
+}
+
+func TestGetCaller(t *testing.T) {
+	var err error
+	assert := assert.New(t)
+
+	err = errors.New("test 1")
+	caller := errors.GetCaller(err)
+	assert.NotEqual(nil, caller, "caller is nil")
+
+	err = nil
+	caller = errors.GetCaller(err)
+	assert.Equal(nil, caller, "caller is not nil")
+}
+
+func TestMarshaller(t *testing.T) {
+	var err error
+	assert := assert.New(t)
+
+	err = errors.New("test 1")
+	byts, err := json.Marshal(err)
+	assert.Equal(nil, err, "err is not nil")
+	assert.Equal(
+		"[{\"caller\":\"#0 error_test.go:130 (github.com/bdlm/errors_test.TestMarshaller)\",\"error\":\"test 1\"}]",
+		string(byts),
+		"JSON did not encode properly",
+	)
 }
