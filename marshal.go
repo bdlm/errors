0728dbe2904ad0e724a9524e2d45aa0edeebee89
Michael Kenney <mkenney@webbedlam.com>
2020-04-30 15:48:32 -0600

    go modules

--- marshal.go
+++ marshal.go
@@ -12,17 +12,16 @@ func (e E) MarshalJSON() ([]byte, error) {
 	jsonData := []map[string]interface{}{}
 
 	for a, b := range list(e) {
-		err, ok := b.(E)
-		if !ok {
-			break
-		}
 		data := map[string]interface{}{}
-		data["caller"] = fmt.Sprintf("#%d %s:%d (%s)",
-			a,
-			path.Base(err.Caller().File()),
-			err.Caller().Line(),
-			runtime.FuncForPC(err.Caller().Pc()).Name(),
-		)
+		err, ok := b.(E)
+		if ok {
+			data["caller"] = fmt.Sprintf("#%d %s:%d (%s)",
+				a,
+				path.Base(err.Caller().File()),
+				err.Caller().Line(),
+				runtime.FuncForPC(err.Caller().Pc()).Name(),
+			)
+		}
 		if "" != err.Error() {
 			data["error"] = err.Error()
 		}
